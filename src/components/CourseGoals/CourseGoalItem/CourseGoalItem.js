58d3e2c88cc1132be5c93737be1742c556a05050
Kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2021-10-26 23:39:56 -1000

    added new css methods

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -3,18 +3,22 @@ import React from 'react';
 import './CourseGoalItem.css';
 
 const CourseGoalItem = props => {
-  // const [deleteText, setDeleteText] = useState('');
+console.log(props); // zdes' inputka jazylgan znacheniyalardy chygaryp jatat
 
   const deleteHandler = () => {
-    // setDeleteText('(Deleted!)');
+
+    // console.log(props.onDelete); // kogda my najimaem na input values(inputan chykkan znacheniyalarga) ,to udalyaet ix
     props.onDelete(props.id);
   };
+  console.log(props.children);
 
   return (
     <li className="goal-item" onClick={deleteHandler}>
       {props.children}
+      
     </li>
   );
 };
+
 
 export default CourseGoalItem;
